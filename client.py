536a4797f7af7c5238171d4fe53a1eb6afd8f9a5
cepheus00 <50025367+cepheus00@users.noreply.github.com>
2019-04-26 16:50:44 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,79 @@
+﻿from tkinter import *
+from getpass import *
+import socket, sys, threading, hashlib
+
+pseudo = input("Entrez votre pseudo : ")
+host = input("Entrez l'adresse IP du serveur : ")
+port = 12800
+password = getpass("Entrez le mot de passe du serveur : ")
+password = password.encode()
+password = hashlib.sha1(password).hexdigest()
+
+connexion = socket.socket()
+
+class ThreadReception(threading.Thread):
+    """objet thread gérant la réception des messages"""
+    def __init__(self, connexion):
+        threading.Thread.__init__(self)
+        self.connexion = connexion
+
+    def run(self):
+        while 1:
+            try:
+                message_recu = self.connexion.recv(1024)
+            except:
+                messages.insert('end', "Connexion avec le serveur perdue...")
+                break
+            message_recu = message_recu.decode()
+            messages.insert('end', message_recu)
+
+def envoyer(texteEntree, messages, pseudo, connexion):
+    message = texteEntree.get()
+    texteEntree.set('')
+    if message != '' and message != "Entrez votre message ici...":
+        messages.insert('end', "%s : %s" % (pseudo, message))
+        try:
+            connexion.send(message.encode())
+        except:
+            messages.insert('end', "Connexion avec le serveur perdue...")
+
+#Initialisation de la fenêtre
+fenetre = Tk()
+fenetre.geometry("720x480")
+fenetre.title("%s@%s - MSJ"%(pseudo, host))
+fenetre.iconbitmap(r'icon.ico')
+
+panneau = PanedWindow(fenetre, orient=VERTICAL)
+
+panneau.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
+
+messages = Listbox(panneau)
+panneau.add(messages, height=380)
+
+texteEntree = StringVar()
+texteEntree.set("Entrez votre message ici...")
+entreeMessage = Entry(panneau, textvariable=texteEntree, width=200)
+panneau.add(entreeMessage, height=50)
+
+boutonEnvoi = Button(panneau, text="Envoyer", command= lambda: envoyer(texteEntree, messages, pseudo, connexion))
+boutonEnvoi.pack(side=RIGHT)
+panneau.add(boutonEnvoi, height=50)
+
+panneau.pack()
+
+#Établissement de la connexion :
+connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    connexion.connect((host, port))
+except socket.error:
+    messages.insert('end', "La connexion a échoué.")
+    sys.exit()
+messages.insert('end', "Connexion établie avec le serveur.")
+
+connexion.send(pseudo.encode()) #Envoi du pseudo
+connexion.send(password.encode()) #Envoi du mot de passe
+
+#Lancement de l'écoute
+th_R = ThreadReception(connexion)
+th_R.start()
+fenetre.mainloop()
---
+++ serveur.py
@@ -0,0 +1,83 @@
+HOST = "localhost"
+PORT = 12800
+
+import socket, sys, threading, hashlib
+from getpass import getpass
+
+class ThreadClient(threading.Thread):
+    '''dérivation d'un objet thread pour gérer la connexion avec un client'''
+    def __init__(self, conn, nom):
+        threading.Thread.__init__(self)
+        self.connexion = conn
+        self.nom = nom
+        
+    def run(self):
+        # Dialogue avec le client :
+        while 1:
+            try:
+                msgClient = self.connexion.recv(1024)
+            except:
+                self.connexion.close()
+                del conn_client[self.nom]
+                print("Client %s déconnecté" % self.nom)
+                for cle in conn_client:
+                    if cle != self.nom:      # ne pas le renvoyer à l'émetteur
+                        messageDeconnexion = "** %s s'est déconnecté **" % self.nom
+                        conn_client[cle].send(messageDeconnexion.encode())
+                break
+            msgClient = msgClient.decode()
+            message = "%s : %s" % (self.nom, msgClient)
+            print(message)
+            # Faire suivre le message à tous les autres clients :
+            for cle in conn_client:
+                if cle != self.nom:      # ne pas le renvoyer à l'émetteur
+                    conn_client[cle].send(message.encode())
+
+# Protection du serveur
+password = getpass("Entrez un mot de passe pour le serveur : ")
+password = password.encode()
+password = hashlib.sha1(password).hexdigest()
+
+# Initialisation du serveur - Mise en place du socket :
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    mySocket.bind((HOST, PORT))
+except socket.error:
+    print("La liaison du socket à l'adresse choisie a échoué.")
+    sys.exit()
+print("Serveur prêt, en attente de requêtes ...")
+mySocket.listen(5)
+
+# Attente et prise en charge des connexions demandées par les clients :
+conn_client = {}                # dictionnaire des connexions clients
+while 1:    
+    connexion, adresse = mySocket.accept()
+    it = connexion.recv(1024)
+    it = it.decode()
+
+    # Vérification du mot de passe :
+    userPass = connexion.recv(1024)
+    userPass = userPass.decode()
+
+    locked = 1
+
+    if userPass == password:
+        locked = 0
+
+    if locked == 0:
+        # Créer un nouvel objet thread pour gérer la connexion :
+        th = ThreadClient(connexion, it)
+        th.start()
+        # Mémoriser la connexion dans le dictionnaire : 
+        conn_client[it] = connexion
+        print("Client %s connecté, adresse IP %s, port %s." % (it, adresse[0], adresse[1]))
+        # Dialogue avec le(s) client(s) :
+        for cle in conn_client:
+            messageConnexion = "** %s s'est connecté **" % it
+            conn_client[cle].send(messageConnexion.encode())
+            if cle != it:
+                messageConnectes = "** %s est connecté **" % cle
+                conn_client[it].send(messageConnectes.encode())
+    else:
+        connexion.send(b"Mot de passe incorrect...")
+        connexion.close()
